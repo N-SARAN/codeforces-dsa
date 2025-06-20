d1ce83761d8d77792a8e8367479d1356b3c873d2
N_SARAN <n.sarancs@gmail.com>
2025-06-21 03:22:59 +0530

    Codeforces | 21 June session

--- B_Shoe_Shuffling.cpp
+++ B_Shoe_Shuffling.cpp
@@ -1,48 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 int main () {
     int t;
     cin >> t;
-    while (t--) {
-        int n;
+    while(t--) {
+        int n, s;
         cin >> n;
+        unordered_map<long long, int> map;
         vector<pair<int,int>> p(n);
-        for (int i = 0; i < n; i++) {
-            p[i].first = i + 1;
+        bool flag = false;
+        for(int i=0; i<n; i++) {
+            p[i].first = i+1;
             cin >> p[i].second;
+            map[p[i].second]++;
         }
 
-        // Frequency check using sorted property
-        bool has_single = false;
-        int count = 1;
-        for (int i = 1; i <= n; i++) {
-            if (i < n && p[i].second == p[i-1].second) {
-                count++;
-            } else {
-                if (count == 1) has_single = true;
-                count = 1;
+        for(auto& [key, val] : map) {
+            if(val == 1) {
+                flag = true;
+                break;
             }
         }
-
-        if (has_single) {
-            cout << -1 << endl;
+        if(flag) {
+            cout << -1;
             continue;
         }
-
         int left = 0, right = 0;
-        while (right < n) {
-            if (p[left].second == p[right].second)
+
+        while(right < n) {
+            if(p[left].second == p[right].second) {
                 right++;
-            else {
-                rotate(p.begin() + left, p.begin() + left + 1, p.begin() + right);
+            }else{
+                rotate(p.begin()+left, p.begin() + left + 1, p.begin() + right);
                 left = right;
             }
         }
-        rotate(p.begin() + left, p.begin() + left + 1, p.begin() + right);
-
-        for (auto& [index, _] : p)
-            cout << index << " ";
+        rotate(p.begin()+left, p.begin() + left + 1, p.begin() + right);
+        for(auto& [pos, val] : p) {
+            cout << pos << " ";
+        }
         cout << endl;
     }
 }
